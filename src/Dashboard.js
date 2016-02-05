eb82486724184e69ad2c652096b6a31b35cf6f5c
Devin Pastoor <devin.pastoor@gmail.com>
2016-02-04 16:04:13 -0800

    add plotly dashboard for starcounts and open issue counts

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -23,34 +23,12 @@ let paperStyles = {
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-     var trace1 = {
-      x: [1, 2, 3, 4],
-      y: [10, 15, 13, 17],
-      mode: 'markers',
-      type: 'scatter'
-    };
-
-    var trace2 = {
-      x: [2, 3, 4, 5],
-      y: [16, 5, 11, 9],
-      mode: 'lines',
-      type: 'scatter'
-    };
-
-    var trace3 = {
-      x: [1, 2, 3, 4],
-      y: [12, 9, 15, 12],
-      mode: 'lines+markers',
-      type: 'scatter'
-    };
-
-    var data1 = [trace1, trace2, trace3];
-    var data2 = [trace1, trace2];
+    var data = require('./fixtures/mockData');
     this.state = {
-      data: [data1, data2],
+      data: data,
       visualizations: [
-        {value: "0", label: "data1"},
-        {value: "1", label: "data2"}
+        {value: "data2", label: "Open Issues Count"},
+        {value: "data1", label: "Star count over time"}
       ]
     }
   }
@@ -70,14 +48,15 @@ export default class App extends React.Component {
   filterViz(val) {
     console.log(val);
     console.log(typeof val);
-    Plotly.newPlot('myDiv', this.state.data[parseInt(val)]);
+    Plotly.newPlot('myDiv', this.state.data[val].x.data);
   }
 
 
   componentDidMount() {
-    Plotly.newPlot('myDiv', this.state.data[1]);
+    Plotly.newPlot('myDiv', this.state.data.data1.x.data);
   }
   render() {
+    console.log(this.state.data)
     return (
       <div>
         <AppBar
@@ -92,7 +71,7 @@ export default class App extends React.Component {
         }}>
           <Select
             name="form-field-name"
-            value="1"
+            value="data1"
             options={this.state.visualizations}
             onChange={this.filterViz.bind(this)}
           />
