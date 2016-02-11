15f158b4d5c959af8376c4693ae761aeedfc2939
Devin Pastoor <devin.pastoor@gmail.com>
2016-02-10 23:55:29 -0800

    substantial UI updates including language-specific fonts

--- src/components/top-chart-list.js
+++ src/components/top-chart-list.js
@@ -4,12 +4,71 @@
 import React from 'react';
 import {Card, List, ListItem, Paper} from 'material-ui';
 import _ from 'lodash';
+
+const languageIcons = {
+  "C": "devicon-c-line-wordmark",
+  "COFFEESCRIPT": "devicon-coffeescript-original",
+  "C+": "devicon-cplusplus-line",
+  "C#": "devicon-csharp-line",
+  "CSS": "devicon-css3-plain",
+  "DOCKER": "devicon-docker-plain",
+  "DOTNET": "devicon-dot-net-plain",
+  "ERLANG": "devicon-erlang-plain",
+  "GO": "devicon-go-plain",
+  "HTML": "devicon-html5-plain",
+  "JAVA": "devicon-java-plain",
+  "JAVASCRIPT": "devicon-nodejs-plain",
+  "MYSQL": "devicon-mysql-plain",
+  "PHP": "devicon-php-plain",
+  "POSTGRESQL": "devicon-postgresql-plain",
+  "PYTHON": "devicon-python-plain",
+  "REDIS": "devicon-redis-plain",
+  "RUBY": "devicon-ruby-plain",
+  "SASS": "devicon-sass-original",
+  "VIML": "devicon-vim-plain"
+  }
 let TopChartList = (props) => {
   let list = _.map(props.data, (d, i) => (
-      <ListItem
-        primaryText={d.name}
-        secondaryText={"Language: " + d.lang }
-      />
+    <div
+      key={d.name}
+    >
+      <div
+        style={{
+          display: "inline-block",
+          margin: 5
+        }}
+      >
+        <span style={{
+        position: "relative",
+        display: "block",
+        fontSize: 16
+        }}
+        onClick={ () => window.open(`https://github.com/${d.name}`)}
+        >
+          {d.name}
+        </span>
+        <span style={{
+        position: "relative",
+        display: "block",
+        color: "#A9A9A9",
+        fontSize: 14,
+        fontWeight: 100
+        }}>
+          { "Stars this week: " + d.stars_in_week }
+        </span>
+      </div>
+      <div
+        style={{
+          display: "inline-block",
+          float: 'right',
+          margin: 10,
+          fontSize: 18
+        }}
+      >
+        {d.language+ "     "}
+        <i className={languageIcons[d.language.toUpperCase()] || "devicon-github-plain"}></i>
+      </div>
+    </div>
     )
   );
   return (
